25089ceaeb474be0eb1d526828479ddd65094841
pungentpaws <whitney_mok@hotmail.com>
2019-09-15 20:07:17 +0200

    Convert an RGB image into different color spaces and generate figures as instructed in part 2 of Assignment 1. The normalized RGB image is still work in progress (see rgb.png, it doesn't match the expectations).

--- assignment1/colorspace/rgb2normedrgb.m
+++ assignment1/colorspace/rgb2normedrgb.m
@@ -1,5 +1,12 @@
 function [output_image] = rgb2normedrgb(input_image)
 % converts an RGB image into normalized rgb
-
+[R, G, B] = getColorChannels(input_image);
+class(R)
+size(R)
+absolute_intensity = R + G + B;
+r = R / absolute_intensity;
+g = G / absolute_intensity;
+b = B / absolute_intensity;
+output_image = cat(3, r, g, b);
 end
 
