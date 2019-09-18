8b97143081072ca7c620cf9aebdde5de9ae1475e
Pungent Paws <44074078+Pungent-Paws@users.noreply.github.com>
2019-09-18 19:31:04 +0200

    Add files via upload
    Remove some useless comments (from earlier, failed attempts) -> final code, ready for submission

--- assignment1/intrinsic_images/recoloring.m
+++ assignment1/intrinsic_images/recoloring.m
@@ -16,23 +16,3 @@ reconstruction = double(copy) .* double(shading);
 % Plot the original ball and the recolored ball
 subplot(1,2,1), imshow(original);
 subplot(1,2,2), imshow(uint16(reconstruction));
-
-
-
-
-
-
-
-
-%{
-new = original;
-
-new(:,:,1) = zeros(size(new, 1), size(new, 2));
-new(:,:,2) = ones(size(new, 1), size(new, 2))*255;
-new(:,:,3) = zeros(size(new, 1), size(new, 2));
- 
-
-subplot(2,1,1), imshow(new);
-
- %}
-
