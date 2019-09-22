24b32da5185c9b0896b425a0594dd8785313b7ff
pungentpaws <whitney_mok@hotmail.com>
2019-09-22 18:31:53 +0200

    Script to perform denoising experiments (as instructed in 1. and 2. of Question-7) on two noisy images. The denoise.m function works for box- and median filtering, but still needs the gaussian filter to be implemented (needed for later parts of Question-7).
    Fix one typo in 2DGauss.

--- assignment2/part1_image_enhancement/denoise.m
+++ assignment2/part1_image_enhancement/denoise.m
@@ -1,11 +1,12 @@
 function [ imOut ] = denoise( image, kernel_type, varargin)
-
+varargin = cell2mat(varargin); % Ensure the input are numeric and not cell array type
 switch kernel_type
     case 'box'
-        fprintf('Not implemented\n')
+        imOut = imboxfilt(image, varargin);  
     case 'median'
-        fprintf('Not implemented\n')
+        imOut = medfilt2(image,[varargin varargin]);
     case 'gaussian'
-        fprintf('Not implemented\n')
+        kernel = gauss2D(varargin(1), varargin(2)); % assume that the 1st argument is sigma, the 2nd one the kernel size
+        %imOut = % convolution formula
 end
 end
