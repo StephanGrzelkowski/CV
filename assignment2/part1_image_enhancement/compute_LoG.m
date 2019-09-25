faead1e13ddcfb393ec59af81845aca2da3dad7b
stephan <stephan.grzelkowski@gmail.com>
2019-09-25 22:57:19 +0200

    Some changes to LoG

--- assignment2/part1_image_enhancement/compute_LoG.m
+++ assignment2/part1_image_enhancement/compute_LoG.m
@@ -8,7 +8,7 @@ switch LOG_type
         % Then apply the Laplacian to the smoothed image
         gauss = fspecial('gaussian', kernel_size, sigma);
         image_smooth = imfilter(image, gauss, 'replicate', 'conv');
-        laplacian = fspecial('laplacian');
+        laplacian = fspecial('laplacian', 0);
         imOut = imfilter(image_smooth, laplacian, 'replicate','conv');
 
     case 2 
@@ -17,13 +17,15 @@ switch LOG_type
         imOut = imfilter(image, LoG, 'replicate', 'conv');
 
     case 3
-        % First create two gauss kernels to smooth the image
-        % Then calculate the difference between the smoothed images
-        gauss_1 = fspecial('gaussian', kernel_size, sigma)
-        gauss_2 = fspecial('gaussian', kernel_size, sigma * 1.6)
-        DoG_1 = imfilter(image, gauss_1, 'conv');
-        DoG_2 = imfilter(image, gauss_2, 'conv');
-        imOut = DoG_1 - DoG_2;
+        % First create two gauss kernels take the DoG and then filter the
+        % image.
+        sigma_1 = 0.5;
+        ratio = 5;
+        gauss_1 = fspecial('gaussian', kernel_size, sigma_1);
+        gauss_2 = fspecial('gaussian', kernel_size, sigma_1 * ratio);
+        dog = gauss_2 - gauss_1;
+        imOut = imfilter(image, dog, 'conv');
+        
 end
 end
 
--- assignment2/part1_image_enhancement/test.m
+++ assignment2/part1_image_enhancement/test.m
@@ -1,7 +1,7 @@
 % Desktop
 % image = imread('D:\Google accounts\Student account\School\Master AI\Computer Vision 1\Lab assignments\Assignment 2\part1_image_enhancement\images\image2.jpg');
 % Laptop
-image = imread('C:\Users\Sander\Desktop\git\CV\assignment2\part1_image_enhancement\images\image2.jpg');
+%image = imread('C:\Users\Sander\Desktop\git\CV\assignment2\part1_image_enhancement\images\image2.jpg');
 % [Gx, Gy, im_magnitude,im_direction] = compute_gradient(image);
 
 % imwrite(Gx, 'Q8_gx.png');
@@ -13,6 +13,7 @@ imOut1 = compute_LoG(image, 1);
 imOut2 = compute_LoG(image, 2);
 imOut3 = compute_LoG(image, 3);
 
+figure;
 subplot(1,3,1);
 imshow(imOut1);
 subplot(1,3,2);
@@ -20,6 +21,7 @@ imshow(imOut2);
 subplot(1,3,3);
 imshow(imOut3);
 
-% imwrite(uint8(imOut1), 'Q9_imout1.png');
-% imwrite(uint8(imOut2), 'Q9_imout2.png');
-% imwrite(uint8(imOut3), 'Q9_imout3.png');
+%save the images
+% imwrite(imOut1, 'Q9_imout1.png');
+% imwrite(imOut2, 'Q9_imout2.png');
+% imwrite(imOut3, 'Q9_imout3.png');
