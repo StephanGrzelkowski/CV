2636e09cf847cb1e031bfd207790c3e192d790fd
toetsie09 <sander.brinkhuijsen@student.uva.nl>
2019-09-30 19:44:07 +0200

    rotation+images

--- assignment3/harris_demo.m
+++ assignment3/harris_demo.m
@@ -2,23 +2,34 @@ image_toy = imread('.\person_toy\00000001.jpg');
 image_pingpong = imread('.\pingpong\0000.jpeg');
 
 % Toy person image
-kernel_size = 7;
+kernel_size = 5;
 sigma = 1;
-threshold = 1;
-[H, r, c] = harris_corner_detector(image_pingpong, kernel_size, sigma, threshold);
-% pause(0.01);
-% threshold = 10;
+
+% threshold = 1;
 % [H, r, c] = harris_corner_detector(image_toy, kernel_size, sigma, threshold);
-% pause(0.01);
+% pause(1)
+threshold = 10;
+[H, r, c] = harris_corner_detector(image_toy, kernel_size, sigma, threshold);
+pause(1)
 % threshold = 100;
 % [H, r, c] = harris_corner_detector(image_toy, kernel_size, sigma, threshold);
-% 
-% % Pingpong image
+% pause(1)
+
+% Pingpong image
 % threshold = 1;
 % [H, r, c] = harris_corner_detector(image_pingpong, kernel_size, sigma, threshold);
-% pause(0.01);
-% threshold = 10;
-% [H, r, c] = harris_corner_detector(image_pingpong, kernel_size, sigma, threshold);
-% pause(0.01);
+% pause(1)
+threshold = 10;
+[H, r, c] = harris_corner_detector(image_pingpong, kernel_size, sigma, threshold);
+pause(1)
 % threshold = 100;
 % [H, r, c] = harris_corner_detector(image_pingpong, kernel_size, sigma, threshold);
+% pause(1)
+
+% Testing rotation-invariant
+threshold = 10;
+image_toy_45 = imrotate(image_toy, 45);
+[H, r, c] = harris_corner_detector(image_toy_45, kernel_size, sigma, threshold);
+pause(1)
+image_toy_90 = imrotate(image_toy, 90);
+[H, r, c] = harris_corner_detector(image_toy_90, kernel_size, sigma, threshold);
