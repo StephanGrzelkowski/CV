e9a705f8c769afd73f454e00a7e36595219b0e57
Sebastiaann <sebastiaanbarneveld@hotmail.com>
2019-10-09 20:48:51 +0200

    af?

--- assignment4/stitch.m
+++ assignment4/stitch.m
@@ -20,18 +20,18 @@ xy_subset = f1((1:2) , best_m(1,:))
 xy_subset2 = f2((1:2), best_m(2,:))
 
 % calculate the difference between the images in their respective axis
-diffx = round(vecnorm(xy_subset(:,1) - xy_subset2(:,1))/2)
-diffy = round(vecnorm(xy_subset(:,2) - xy_subset2(:,2))*2)
+diffx = round(vecnorm(xy_subset(:,1) - xy_subset2(:,1))/2);
+diffy = round(vecnorm(xy_subset(:,2) - xy_subset2(:,2))*2)-50;
 
 % Calculate where tformedIm should be overlayed on im1
 s1 = size(im1);
 s2 = size(tformedIm);
  m = best_t(1:2,1:2);
  t = best_t(3, 1:2);
-[width_tformedIm, height_tformedIm] = Whitney(size(im2,1), size(im2,2), m, t)
+[width_tformedIm, height_tformedIm] = Whitney(size(im2,1), size(im2,2), m, t);
 
 % Make a new image of the correct size and insert im1 and tformedIm
-nn = zeros(size(im1, 1)+width_tformedIm-diffy, size(im1,2)+height_tformedIm-diffx)
+nn = zeros(size(im1, 1)+width_tformedIm-diffy, size(im1,2)+height_tformedIm-diffx);
 nn(1:s1(1),1:s1(2)) = im1;
 nn(1+s1(1)-diffy: s1(1)-diffy+s2(1), s1(2) - diffx+1:  s1(2) - diffx+s2(2))= tformedIm;
 
